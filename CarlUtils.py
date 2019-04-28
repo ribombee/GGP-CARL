641d97020ac4beabca2e99031e7ea9ed7a16113a
Valli <valgardur16@ru.is>
2019-04-28 18:03:11 +0000

    changed Sarsa selection policy to use node visits as threshold

--- CarlUtils.py
+++ CarlUtils.py
@@ -97,19 +97,18 @@ class SarsaSelectionPolicy(Policy):
         best_action = -1
         best_sucb = -float("inf")
 
-        for action_index in current_node.actions[role_index]:
-            action = current_node.actions[role_index][action_index]
-            current_sucb = 0 
-            if action.N < self.sucb_threshold:
-                #What is the range of this? 0-100? yes?
-                #TODO: find out how to use sarsa values in ucb (they are very low)
-                #val = self.sarsa_agents[role_index].value(current_state, action_index)
-                #current_sucb = ucb(current_node, val, action.N)
-
-
-                best_action = self.sarsa_agents[role_index].nondet_policy(current_state, sm.get_legal_state(role_index))
-                break
-            else:
+        if current_node.N < self.sucb_threshold:
+            best_action = self.sarsa_agents[role_index].nondet_policy(current_state, sm.get_legal_state(role_index))
+        else:
+            for action_index in current_node.actions[role_index]:
+                action = current_node.actions[role_index][action_index]
+                current_sucb = 0 
+                #if action.N < self.sucb_threshold:
+                    #What is the range of this? 0-100? yes?
+                    #TODO: find out how to use sarsa values in ucb (they are very low)
+                    #val = self.sarsa_agents[role_index].value(current_state, action_index)
+                    #current_sucb = ucb(current_node, val, action.N)
+                
                 current_sucb = ucb(current_node, action.Q, action.N)
                 if current_sucb > best_sucb:
                     best_sucb = current_sucb
