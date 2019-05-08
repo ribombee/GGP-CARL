4791483b8ffebb79b38a1e431fda3b5708fd059b
Valli <valgardur16@ru.is>
2019-05-08 12:55:44 +0000

    removed stray newlines in environment variable declarations that were causing trouble

--- SetupScript.sh
+++ SetupScript.sh
@@ -15,7 +15,7 @@ mkdir ~/Documents/CarlAgent
 export K273_PATH=~/Documents/CarlAgent/k273
 export GGPLIB_PATH=~/Documents/CarlAgent/ggplib
 export GGP_BASE_PATH=$GGPLIB_PATH/ggp-base
-export LD_LIBRARY_PATH=$K273_PATH/build/lib:$GGPLIB_PATH/src/cpp:$LD_LIBRARY_PATH\n
+export LD_LIBRARY_PATH=$K273_PATH/build/lib:$GGPLIB_PATH/src/cpp:$LD_LIBRARY_PATH
 export CLASSPATH=$GGP_BASE_PATH/build/classes/main:$GGP_BASE_PATH/build/resources/main:$GGP_BASE_PATH/lib/Guava/guava-14.0.1.jar:$GGP_BASE_PATH/lib/Jython/jython.jar:$GGP_BASE_PATH/lib/Clojure/clojure.jar:$GGP_BASE_PATH/lib/Batik/batik-1.7.jar:$GGP_BASE_PATH/lib/FlyingSaucer/core-renderer.jar:$GGP_BASE_PATH/lib/javassist/javassist.jar:$GGP_BASE_PATH/lib/reflections/reflections-0.9.9-RC1.jar:$GGP_BASE_PATH/lib/Htmlparser/htmlparser-1.4.jar
 export PYTHONPATH=$GGPLIB_PATH/src:$PYTHONPATH
 export PATH=$GGPLIB_PATH/bin:$PATH
@@ -23,7 +23,7 @@ export PATH=$GGPLIB_PATH/bin:$PATH
 echo 'export K273_PATH=~/Documents/CarlAgent/k273
 export GGPLIB_PATH=~/Documents/CarlAgent/ggplib
 export GGP_BASE_PATH=$GGPLIB_PATH/ggp-base
-export LD_LIBRARY_PATH=$K273_PATH/build/lib:$GGPLIB_PATH/src/cpp:$LD_LIBRARY_PATH\n
+export LD_LIBRARY_PATH=$K273_PATH/build/lib:$GGPLIB_PATH/src/cpp:$LD_LIBRARY_PATH
 export CLASSPATH=$GGP_BASE_PATH/build/classes/main:$GGP_BASE_PATH/build/resources/main:$GGP_BASE_PATH/lib/Guava/guava-14.0.1.jar:$GGP_BASE_PATH/lib/Jython/jython.jar:$GGP_BASE_PATH/lib/Clojure/clojure.jar:$GGP_BASE_PATH/lib/Batik/batik-1.7.jar:$GGP_BASE_PATH/lib/FlyingSaucer/core-renderer.jar:$GGP_BASE_PATH/lib/javassist/javassist.jar:$GGP_BASE_PATH/lib/reflections/reflections-0.9.9-RC1.jar:$GGP_BASE_PATH/lib/Htmlparser/htmlparser-1.4.jar
 export PYTHONPATH=$GGPLIB_PATH/src:$PYTHONPATH
 export PATH=$GGPLIB_PATH/bin:$PATH' >> ~/.bashrc
