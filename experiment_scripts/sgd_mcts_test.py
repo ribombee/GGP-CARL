715de7d1fae02bc9501b66fc6e0e9225b1f26976
Valli <valgardur16@ru.is>
2019-05-09 17:10:13 +0000

    added breakthrough as a game in expansion test

--- experiment_scripts/sgd_mcts_test.py
+++ experiment_scripts/sgd_mcts_test.py
@@ -2,7 +2,7 @@ import sys, subprocess
 from BatchGameRunner import BatchGameRunner
 from BatchGameRunner import Player_info
 
-game = "connectFour"
+games = ["connectFour", "breakthrough"]
 start_clock = 600
 play_clock = "99999"
 
@@ -21,9 +21,9 @@ expansions_start = 500
 expansions_fin = 5000
 expansions_step = 1000
 
-runs_per_step = 20
-
-for expansions in range(expansions_start, expansions_fin, expansions_step):
-    game_runner = BatchGameRunner()
-    game_runner.setup(game, str(start_clock), play_clock, Player_info(p1_ip, p1_type, p1_port, p1_regressor), Player_info(p2_ip, p2_type, p2_port, p2_regressor), expansions)
-    game_runner.run_tests(runs_per_step)
+runs_per_step = 10
+for game in games:
+    for expansions in range(expansions_start, expansions_fin, expansions_step):
+        game_runner = BatchGameRunner()
+        game_runner.setup(game, str(start_clock), play_clock, Player_info(p1_ip, p1_type, p1_port, p1_regressor), Player_info(p2_ip, p2_type, p2_port, p2_regressor), expansions)
+        game_runner.run_tests(runs_per_step)
