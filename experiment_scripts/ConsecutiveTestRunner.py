fd3299a325481ad19812a0f368dcc2782c9053cb
Valli <valgardur16@ru.is>
2019-05-11 11:26:46 +0000

    added error logging to ConsecutiveTestRunner

--- experiment_scripts/ConsecutiveTestRunner.py
+++ experiment_scripts/ConsecutiveTestRunner.py
@@ -1,11 +1,19 @@
-import sys, time, threading
+import sys, time, threading, logging, traceback
 from regressor_test import Regressor_test
 
+lock = threading.Lock()
+
 def regression_test_launcher(ip1, ip2):
-    test = Regressor_test(ip1, ip2)
-    test.start_test()
+    try:
+        test = Regressor_test(ip1, ip2)
+        test.start_test()
+    except:
+        logging.exception("\n***************************************\n")
+
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='errors.log',level=logging.DEBUG)
+
     ips_path = sys.argv[1]
     ip_sets = []
     with open(ips_path, 'r') as ip_file:
@@ -19,7 +27,7 @@ if __name__ == "__main__":
         print "starting thread " + str(index)
         thread = threading.Thread(target=regression_test_launcher, args=(ip_set[0],ip_set[1]))
         thread.start()
-        time.sleep(15)
+        time.sleep(1)
         thread_pool.append(thread)
 
     for index, thread in enumerate(thread_pool):
